d101a8a319149e6ea1cf13df05c66d2fe181cff6
Lander990 <74200309+Lander990@users.noreply.github.com>
2021-04-14 18:29:01 +0200

    [FIXED]: No cargaban los scripts

--- PruebasProyecto/index.js
+++ PruebasProyecto/index.js
@@ -1,6 +1,10 @@
-if (sessionStorage.getItem("nomUser") !== null) {
+function cargarUser() {
+	if (sessionStorage.getItem("nomUser") !== null) {
 	$("#vacio").css("display","inherit");
 	document.getElementById('vacio').innerHTML = "Perfil de " + sessionStorage.getItem("nomUser");
 	$("#vacio").css("border","1px solid");
 	$("#vacio").css("border-color","white");
 }
+}
+
+
