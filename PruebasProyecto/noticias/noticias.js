6c8d9d017349f0dbb4c975e8380b868abe01e5e9
Lander990 <74200309+Lander990@users.noreply.github.com>
2021-04-20 17:04:30 +0200

    Terminada la sección de noticias

--- PruebasProyecto/noticias/noticias.js
+++ PruebasProyecto/noticias/noticias.js
@@ -1,6 +1,11 @@
-if (sessionStorage.getItem("nomUser") !== null) {
+function cargarUser() {
+	if (sessionStorage.getItem("nomUser") !== null) {
 	$("#vacio").css("display","inherit");
 	document.getElementById('vacio').innerHTML = "Perfil de " + sessionStorage.getItem("nomUser");
 	$("#vacio").css("border","1px solid");
 	$("#vacio").css("border-color","white");
+	$(".zona-anuncios").css("display","none");
 }
+}
+
+
