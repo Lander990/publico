de2813c0dea6c3e39b6d3ec664de5aca7f2f23b7
Lander990 <74200309+Lander990@users.noreply.github.com>
2021-05-28 01:13:11 +0200

    Se han agregado los diagramas a la pagina web

---
+++ PruebasProyecto/diagrama/diagrama.js
@@ -0,0 +1,30 @@
+function cargarUser() {
+	if (sessionStorage.getItem("nomUser") !== null) {
+	$("#vacio").css("display","inherit");
+	document.getElementById('vacio').innerHTML = "Perfil de " + sessionStorage.getItem("nomUser");
+	$("#vacio").css("border","1px solid");
+	$("#vacio").css("border-color","white");
+
+	$("#login").css("display","none");
+	$("#logout").css("display","initial");
+
+}
+}
+
+function logout() {
+	sessionStorage.removeItem("nomUser");
+	window.alert("Logout con exito!");
+	$("#vacio").css("display","none");
+
+	$("#login").css("display","initial");
+	$("#logout").css("display","none");
+
+}
+
+function abrirVideo()	 {
+	$("#myModal").css("display","block");
+}
+
+function cerrarVideo()	{
+	$("#myModal").css("display","none");
+}
